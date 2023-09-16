98111c7793bbe9db07e0b4c88bcf9ebdfa19094e
mateus-alb <135767932+mateus-alb@users.noreply.github.com>
2023-09-16 12:02:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+
+    fabric.Image.fromURL('AniversarioImagem.png', function (Img) { blockImageObject Img; =
+
+        blockImageObject.scaleToWidth(700);
+        
+        blockImageObject.scaleToHeight(510);
+        
+        blockImageObject.set({
+        
+        top:0,
+        
+        left:0
+        
+        });
+        
+        canvas.add(block ImageObject);
+        
+        });
+
+
+
+	
+
+
+}
+
+function playSound(){
+
+}
